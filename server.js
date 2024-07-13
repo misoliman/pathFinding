abb5ee30c0691dd8f35e9577b314b9e52a3aa292
Mohamed Ibrahim Mohamed Osman <mimosman@tamgo.com>
2024-07-13 15:42:13 +0300

    add start script

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-const port = 3000 // he had process.env.port
+const port = process.env.port || 3000
 
 app.use(express.static(path.join(__dirname, "public")))
 
-// app.get("/", (req, res) => {
-//     res.sendFile("public/home.html")
-// })
+app.get("/", (req, res) => {
+    res.sendFile("public/home.html")
+})
 
 app.listen(port, () => {
     console.log("Server is up on port", port)
