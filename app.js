da01e9e475fe71d5501a929153c3c6207f4f6409
Mohamed Ibrahim Mohamed Osman <mimosman@tamgo.com>
2024-07-15 19:51:46 +0300

    trying to deploy 1

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express")
 const app = express()
 const path = require("path")
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, "public")))
 
-app.get("/", (req, res) => {
-    res.sendFile("public/home.html")
-})
+// app.get("/", (req, res) => {
+//     res.sendFile("public/home.html")
+// })
 
 app.listen(port, () => {
-    console.log("Server is up on port", port)
+    console.log("Server is up on port", PORT)
 })
